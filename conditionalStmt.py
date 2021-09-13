5ea343fb42c0764fdc7b39f883f42df552851ee8
Jcoder2022 <JCoder2022@gmail.com>
2021-09-13 20:02:22 +0100

    ADDED Multiple conditions and, or

--- conditionalStmt.py
+++ conditionalStmt.py
@@ -56,3 +56,34 @@ else:
 
     # o/p will be following
     # It's a lovely day
+
+# Multiple Conditions
+#   AND,  OR
+# if applicant has high income AND good credit
+#   Eligible for loan
+
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+
+# if applicant has high income OR good credit
+#   Eligible for loan
+
+has_high_income = True
+has_good_credit = False
+
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+
+
+
+
+
+
+
+
+
+
