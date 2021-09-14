a40bad3d339277b56f0d259566862f4097d089d0
Jcoder2022 <JCoder2022@gmail.com>
2021-09-14 10:49:46 +0100

    if statement, comparison operator logical operators

--- conditionalStmt.py
+++ conditionalStmt.py
@@ -58,7 +58,7 @@ else:
     # It's a lovely day
 
 # Multiple Conditions
-#   AND,  OR
+#   AND,  OR, NOT
 # if applicant has high income AND good credit
 #   Eligible for loan
 
@@ -78,12 +78,58 @@ has_good_credit = False
 if has_high_income or has_good_credit:
     print("Eligible for loan")
 
+# Not - example
+# if applicant has good credit AND doesn't have criminal record
+#   Eligible for loan
+
+has_good_credit = True
+has_criminal_record = True
+
+if has_good_credit and not has_criminal_record:
+    print ("Eligible for loan")
+
+# Comparison Operator
+#   <,<=,>,>=,==,!=
+# if temperature is greater than 30
+#   it's a hot day
+# otherwise if it's less than 10
+#   it's a cold day
+# otherwise
+#   it's neither hot nor cold
+
+temperature = 10
+
+if temperature > 30:
+    print("it's a hot day")
+elif temperature<10:
+    print("it's a cold day")
+else:
+    print("it's neither hot nor cold")
 
 
+# exercise
+
+name="Salman"
+length = len(name)
+if length < 3:
+    print("name must be atleast 3 char long")
+elif length > 50:
+    print("name can be a max of 50 characters")
+else:
+    print("name looks good!")
 
 
+# exercise
 
+#   Weight: 160  (input)
+#   (L)bs or (K)g: l  (input)
+#   You are 72.0 kilos
 
-
-
-
+weight = input("Weight: ")
+measurement_unit = input("(L)bs or (K)g: ")
+if measurement_unit.lower() == 'l':
+    weight_in_kilos = int(weight) * 0.45359237
+    print(f"You are {weight_in_kilos} kilos")
+else:
+    weight_in_pounds = int(weight)/0.45
+    print(f"You are {weight_in_pounds} pounds")
